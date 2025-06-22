6d3a91d526ab0b7fca2a3f953c66ee5fbc1e2ef8
IronMOUSE7 <santoshruchal29@gmail.com>
2025-06-22 22:57:20 +0530

    very imp change for example

--- src/main/java/net/ironmouse/tutorialmod/TutorialMod.java
+++ src/main/java/net/ironmouse/tutorialmod/TutorialMod.java
@@ -28,7 +28,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
-
+//very important comment for example
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod
